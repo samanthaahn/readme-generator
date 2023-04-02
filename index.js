38aba0698ba0752a2fc08b2d9f2c9edf80d7fe9b
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-04-02 07:25:29 -0700

    added my old function back to my index

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
-// TODO: Include packages needed for this application
+const inquirer = required('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const generateReadMe = ({ title, description, installation, usage, contributing, tests, license }) => 
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+`# ${title}
 
-// TODO: Create a function to initialize app
-function init() {}
+## Description
+${description}
 
-// Function call to initialize app
-init();
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## Contributing
+${contributing}
+
+## Tests
+${tests}
+
+## License
+${license}`;
+
+
+
+inquirer
+.prompt([
+{
+    type: 'input',
+    name: 'description',
+    message: 'What is your project about?'
+},
+])
+.then((answer) => {
+    const readMeContent = generateReadMe(answers);
+
+    fs.writeFile('readme.md', readMeContent, (err) =>
+    err ? console.log(err) : console.log('Successfully created read.me file!')
+    );
+});
+
+
+// // TODO: Create an array of questions for user input
+// const questions = [];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
